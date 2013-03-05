516f48e4ad78e80aa53514bce6dc5c9edf0174d7
Cory Virok <coryvirok@gmail.com>
2013-03-04 19:06:21 -0800

    sample php app

---
+++ test.php
@@ -0,0 +1,25 @@
+<?php
+ 
+require_once('rollbar.php');
+ 
+class EchoLogger {
+    public function log($level, $message) {
+        echo "[Rollbar] $level $message\n";
+    }
+}
+ 
+$config = array(
+    'access_token' => '28dd1d28fc1c4ba3a9302fe85fd2f6e2',
+    'environment' => 'test',
+    'root' => '/home/cory/www/php-sample',
+    'logger' => new EchoLogger()
+);
+Rollbar::init($config);
+ 
+try {
+    throw new Exception("hello");
+} catch (Exception $e) {
+    Rollbar::report_exception($e);
+}
+ 
+?>
